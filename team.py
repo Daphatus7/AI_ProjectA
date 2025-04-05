e1e35c4c8043960eee7257d49a0abbfa7e0e62d1
peiyuw2 <peiyuw08@gmail.com>
2025-04-05 19:28:27 +1100

    include team.py

---
+++ team.py
@@ -0,0 +1,15 @@
+{
+    "team_name": "CodingAgents",
+    "team_members": [
+        {
+            "name": "Zeming Xia",
+            "student_id": "1173900",
+            "email": "zemxia@student.unimelb.edu.au"
+        },
+        {
+            "name": "Peiyu Wang",
+            "student_id": "1372456",
+            "email": "peiyuw2@student.unimelb.edu.au"
+        }
+    ]
+}
